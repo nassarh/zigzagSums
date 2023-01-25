699ae9cf742fd9d2ce440988ea56b76badf6feb9
nassarh <35009715+nassarh@users.noreply.github.com>
2023-01-25 17:35:17 -0600

    Add files via upload

---
+++ miuraPillarDifferentOrientation.py
@@ -0,0 +1,43 @@
+# Import stuff
+import numpy as np
+import zigzagsum as qd
+import pyvista as pv  # for 3D rendering
+
+# Define reference unit cell
+# of "equilateral" Miura ori
+# alternative orientation
+gamma = np.pi / 3
+c = np.cos(gamma)
+s = np.sin(gamma)
+u0 = np.array([1, 0, 0])
+v0 = np.array([-1, 0, 0])
+w0 = np.array([c, s, 0])
+wb0 = np.array([-c, s, 0])
+sgnop = -1
+
+# opening angle at initial zigzag
+theta = np.pi / 2
+
+# inclination angle at initial zigzag
+beta = -np.pi / 6
+
+# number of cells per parallel
+N = 30
+
+# number of cells per meridian
+cells = 20
+
+# Define initial zigzag
+# opening theta, inclination beta, rotational symmetry of step  2 pi / N
+u, v, rot, _ = qd.zigcircle(theta, beta, N=N, u0=u0, v0=v0, w0=w0, wb0=wb0, sgnop=sgnop)
+
+# Build pattern: crease vectors and vertices
+U, V, W, _ = qd.manysteps(u, v, u0, v0, w0, wb0, cells, sgnop=sgnop, rot=rot)
+X, Y, Z = qd.integrate(U, V, W, N, per=True, rot=rot)
+
+# plot
+pl = pv.Plotter()
+pl.set_background("black", top="white")
+grid = pv.StructuredGrid(X, Y, Z)
+pl.add_mesh(grid, show_edges=True, line_width=3, color=[193, 225, 193])
+pl.show()
